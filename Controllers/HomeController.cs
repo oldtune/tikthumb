b523bbddef02f5ec5734568717a8e9663bca7706
Do Tran <do.tran@saigontechnology.com>
2024-01-23 06:50:36 +0700

    wip

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,24 @@
 using System.Diagnostics;
-using System.IO;
 using System.Text;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
-using tikthumb.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace tikthumb.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly string _filePath;
+    private readonly string _tempPath;
     private readonly string _outputPath;
     private readonly string _currentTimeStamp;
+    private readonly UploadContext _uploadContext;
 
     public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
-        _filePath = configuration["SaveLocation"];
-        _outputPath = _filePath + "/output";
+        _tempPath = configuration["SaveLocation"];
+        _outputPath = _tempPath + "/output";
         _currentTimeStamp = GetCurrentTimeStamp();
     }
 
@@ -36,83 +36,103 @@ public class HomeController : Controller
     {
         var files = Request.Form.Files;
 
-        var videoFile = await SaveFormFile(files["videoFile"]);
+        var uploadedVideoFile = files["videoFile"];
+        var uploadedThumbnailFile = files["thumbnailFile"];
 
-        var thumbnailFile = await SaveFormFile(files["thumbnailFile"]);
+        var uploadContext = new UploadContext(_tempPath,
+            _currentTimeStamp,
+            uploadedVideoFile!.FileName,
+            uploadedThumbnailFile!.FileName);
 
-        await InsertFrame(videoFile, thumbnailFile);
-        //return stream
+        await SaveFormFile(files["videoFile"]!, uploadContext.VideoSaveInfo);
+
+        var videoSize = await GetVideoSize(uploadContext.VideoSaveInfo.SavedFileNameWithExtension);
+
+        await ResizeAndSaveThumbnail(uploadContext.ImageSaveInfo, uploadedThumbnailFile, videoSize);
+
+        var tempDataContext = new FfmpegContext(_tempPath, _outputPath, uploadContext, _currentTimeStamp);
+
+        await InsertFrame(uploadContext, tempDataContext);
+
         return View();
     }
 
-    private async Task InsertFrame(FileSavedInfo videoFile, FileSavedInfo imageFile)
+    private async Task<Size> GetVideoSize(string videoFileName)
     {
-        await TransferImageIntoVideo(imageFile);
-        //create txt file that contains file list
-        // await CreateInputTextFile(videoFile, imageFile);
-        // //concat stream
-        // await ConcatStream(videoFile, imageFile, MakeFileInfoPath(_outputPath, _currentTimeStamp));
+        var argument = $"-v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 {videoFileName}";
+        var process = CreateFfmpegProcess(argument);
+        process.Start();
+
+        var stdOutput = await process.StandardOutput.ReadToEndAsync();
+        _logger.LogInformation($"Stdoutput is: {stdOutput}");
+
+        var dimensionArray = stdOutput.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList();
+        return new Size(dimensionArray[0], dimensionArray[1]);
     }
 
-    private async Task TransferImageIntoVideo(FileSavedInfo fileInfo)
+    private async Task ResizeAndSaveThumbnail(FileSaveInfo imageSaveInfo, IFormFile file, Size desiredSize)
     {
-        var argument = $"-loop 1 -i {fileInfo.FullSavedPath} -c:v libx264 -t 0.17 -pix_fmt yuv420p {fileInfo.SavedFileNameWithoutExtension}.mp4";
+        using var stream = file.OpenReadStream();
+
+        var image = await Image.LoadAsync(stream);
+
+        if (image.Size != desiredSize)
+        {
+            var width = (desiredSize.Width & 1) == 0 ? desiredSize.Width : desiredSize.Width - 1;
+            var height = (desiredSize.Height & 1) == 0 ? desiredSize.Height : desiredSize.Height - 1;
+
+            image.Mutate(x => x.Resize(width, height));
+        };
+
+        await image.SaveAsync(imageSaveInfo.SavedFileNameWithExtension);
+    }
+
+    private async Task InsertFrame(UploadContext context, FfmpegContext ffmpegContext)
+    {
+        await TransferImageIntoVideo(context.ImageSaveInfo.SavedFileNameWithExtension, ffmpegContext.TempImageVideoFilePath);
+        await CreateInputTextFile(context.VideoSaveInfo.SavedFileNameWithExtension, context.ImageSaveInfo.SavedFileNameWithExtension, ffmpegContext.InputFilePath);
+        await ConcatStream(ffmpegContext);
+    }
+
+    private async Task TransferImageIntoVideo(string imageFullPath, string outputFullPath)
+    {
+        var argument = $"-loop 1 -i {imageFullPath} -c:v libx264 -t 0.17 -pix_fmt yuv420p {outputFullPath}";
         var process = CreateFfmpegProcess(argument);
         process.Start();
         await process.WaitForExitAsync();
     }
 
-    private async Task CreateInputTextFile(FileSavedInfo videoFile, FileSavedInfo imageFile)
+    private async Task CreateInputTextFile(string videoFileFullPath, string imageVideoFileFullPath, string inputFilePath)
     {
-        var outputPath = MakeFileInfoPath(_outputPath, _currentTimeStamp);
-        var content = $"file '{videoFile.FullSavedPath}'\nfile '{_filePath}/{imageFile.SavedFileNameWithoutExtension}.mp4'";
+        var content = $"file '{videoFileFullPath}'\nfile '{imageVideoFileFullPath}'";
 
-        await System.IO.File.WriteAllTextAsync(outputPath, content, Encoding.UTF8);
+        await System.IO.File.WriteAllTextAsync(inputFilePath, content, Encoding.UTF8);
     }
 
-    private async Task ConcatStream(FileSavedInfo videoFile, FileSavedInfo imageFile, string textFile)
+    private async Task ConcatStream(FfmpegContext ffmpegContext)
     {
-        var argument = $"-f concat -i {textFile} -c copy -movflags +faststart {_outputPath}/{videoFile.SavedFileNameWithoutExtension}.mp4";
+        var argument = $"-f concat -i {ffmpegContext.InputFilePath} -c copy -movflags +faststart {ffmpegContext.OutputFilePath}";
         _logger.LogInformation(argument);
         var process = CreateFfmpegProcess(argument);
         process.Start();
         await process.WaitForExitAsync();
     }
 
-    private async Task<FileSavedInfo> SaveFormFile(IFormFile file)
+    private async Task SaveFormFile(IFormFile file, FileSaveInfo fileSaveInfo)
     {
-        var (savePath, newFileName) = GetFilePathToSave(_filePath, file.FileName, _currentTimeStamp);
-        using var videoFileStream = new FileStream(savePath, FileMode.Create);
-        await file.CopyToAsync(videoFileStream);
-
-        return new FileSavedInfo
-        {
-            FileExtension = Path.GetExtension(file.FileName),
-            SavedFileNameWithoutExtension = newFileName,
-            FullSavedPath = savePath
-        };
-    }
-
-    private (string FullPath, string FileName) GetFilePathToSave(string basePath, string fileNameWithExtension, string timeStamp)
-    {
-        var fileExtension = Path.GetExtension(fileNameWithExtension);
-        var fileName = Path.GetFileNameWithoutExtension(fileNameWithExtension);
-        if (fileName.Length > 100)
-        {
-            fileName = fileName.Substring(0, 100);
-        }
-
-        return ($"{basePath}/{timeStamp}_{fileName}{fileExtension}", $"{timeStamp}_{fileName}");
+        using var fileStream = new FileStream(fileSaveInfo.SavedFileNameWithExtension, FileMode.Create);
+        await file.CopyToAsync(fileStream);
     }
 
     private Process CreateFfmpegProcess(string arguments)
     {
         var process = new Process();
 
-        process.StartInfo.UseShellExecute = true;
+        process.StartInfo.UseShellExecute = false;
         process.StartInfo.FileName = "ffmpeg";
         process.StartInfo.Arguments = arguments;
         process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.RedirectStandardOutput = true;
 
         return process;
     }
@@ -123,27 +143,81 @@ public class HomeController : Controller
         return unixTimestamp;
     }
 
-    private string MakeFileInfoPath(string outputPath, string timeStamp)
+    public record UploadContext
     {
-        return $"{outputPath}/fileinfo_{timeStamp}.txt";
+        public FileSaveInfo ImageSaveInfo { set; get; }
+        public FileSaveInfo VideoSaveInfo { set; get; }
+        public FfmpegContext IntermediateFile { set; get; }
+        public string CurrentTimestamp { set; get; }
+
+        public UploadContext(string tempPath,
+            string timeStamp,
+            string videoFileNameWithExtension,
+            string imageFileNameWithExtension)
+        {
+
+            VideoSaveInfo = FilePathHelper.CreateUploadedMediaMetadata(tempPath, videoFileNameWithExtension, timeStamp);
+            ImageSaveInfo = FilePathHelper.CreateUploadedMediaMetadata(tempPath, imageFileNameWithExtension, timeStamp);
+
+            CurrentTimestamp = timeStamp;
+        }
     }
 
-    public class FileSavedInfo
+    public record FileSaveInfo
     {
-        public string OriginalFileNameWithoutExtension { set; get; }
+        public string FileNameWithoutExtension { set; get; }
+        public string FileNameWithExtension { set; get; }
         public string SavedFileNameWithoutExtension { set; get; }
+        public string SavedFileNameWithExtension { set; get; }
         public string FileExtension { set; get; }
-        public string FullSavedPath { set; get; }
+        // public Size Size { set; get; }
     }
 
-    public class VideoInfo
+    public class FfmpegContext
     {
-        public StreamInfo StreamInfo { set; get; }
+        public string TempImageVideoFilePath { set; get; }
+        public string InputFilePath { set; get; }
+        public string OutputFilePath { set; get; }
+        public string CreateInputFileContent(string videoFilePath)
+        {
+            return $"file '{videoFilePath}\nfile '{TempImageVideoFilePath}'";
+        }
+
+        // public string Create
+
+        public FfmpegContext(string tempPath,
+         string outputPath,
+         UploadContext context,
+         string timeStamp)
+        {
+            InputFilePath = FilePathHelper.GetPathToSave(tempPath, FilePathHelper.ConstructFileNameToSave("files_container", timeStamp));
+            TempImageVideoFilePath = FilePathHelper.GetPathToSave(tempPath, $"{FilePathHelper.ConstructFileNameToSave(context.ImageSaveInfo.FileNameWithoutExtension, timeStamp)}.mp4");
+            OutputFilePath = FilePathHelper.GetPathToSave(outputPath, $"{FilePathHelper.ConstructFileNameToSave("output", timeStamp)}.mp4");
+        }
     }
 
-    public class StreamInfo
+    public static class FilePathHelper
     {
-        public int Width { set; get; }
-        public int Height { set; get; }
+
+        public static FileSaveInfo CreateUploadedMediaMetadata(string savePath, string filenameWithExtension, string timeStamp)
+        {
+            return new FileSaveInfo()
+            {
+                FileNameWithExtension = filenameWithExtension,
+                FileNameWithoutExtension = Path.GetFileNameWithoutExtension(filenameWithExtension),
+                FileExtension = Path.GetExtension(filenameWithExtension),
+                SavedFileNameWithExtension = Path.Combine(savePath, ConstructFileNameToSave(filenameWithExtension, timeStamp))
+            };
+        }
+
+        public static string GetPathToSave(string basePath, string fileName)
+        {
+            return Path.Combine(basePath, fileName);
+        }
+
+        public static string ConstructFileNameToSave(string fileName, string timeStamp)
+        {
+            return $"{timeStamp}_{fileName}";
+        }
     }
 }
---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build-env
+WORKDIR /app
+
+COPY . .
+RUN dotnet restore
+RUN dotnet publish -c Release -o out
+
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 as app
+EXPOSE 8080
+RUN apt-get update && apt-get install -y ffmpeg
+WORKDIR /app
+RUN mkdir /app/tmp
+RUN mkdir /app/tmp/out
+COPY --from=build-env /app/out .
+ENTRYPOINT ["dotnet", "tikthumb.dll"]
