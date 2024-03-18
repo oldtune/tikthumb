e8454252ea8e83257926d6011c6412243f02ff98
Do Tran <do.tran@saigontechnology.com>
2024-03-18 15:28:56 +0700

    wip

---
+++ push-ecr-windows.sh
@@ -0,0 +1,6 @@
+import-Module AWSPowerShell.NetCore
+aws sso login
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 298086761063.dkr.ecr.ap-southeast-1.amazonaws.com
+docker build -t tikthumb .
+docker tag tikthumb:latest 298086761063.dkr.ecr.ap-southeast-1.amazonaws.com/tikthumb:latest
+docker push 298086761063.dkr.ecr.ap-southeast-1.amazonaws.com/tikthumb:latest
